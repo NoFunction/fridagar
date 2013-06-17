9d142d65e4c91059b2fbc2dcf4e7bc73c56658a9
Gudjon Jonsson <gudjonj@gmail.com>
2013-06-17 02:41:03 +0000

    Fixed a bug

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once('iCalEvent.class.php');
 
 // Set headers for genuine iCalendar authenticity
 header('Content-type: text/calendar; charset=utf-8');
-header('Content-Disposition: inline; filename=' . __FILE__);
+header('Content-Disposition: inline; filename=fridagar.ics');
 
 // Content, make sure we get latin-1
 $content = file_get_contents('http://www.lanamal.is/fagfjarfestar/fridagar');
@@ -31,11 +31,12 @@ foreach($entries as $entry)
 	$event = $entry->childNodes->item(2)->nodeValue;
 
 	// If current year is not in the date or the event is empty, we skip it
-	if(!strpos($event, date('Y')) || $event == '')
+	if(!strpos($date, date('Y')) || $event == '')
 		continue;
 	
 	// Create the object and add it to our iCal
 	$eventObj = new iCalEvent($date, $event);
+	
 	$iCal->addEvent($eventObj);
 }
 
